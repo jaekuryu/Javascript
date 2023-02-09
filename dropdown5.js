da3f884ba8b40de7e7d35deb99e53837e3710cf6
Sharetechnote <jaekuryu@gmail.com>
2023-02-08 20:04:01 -0500

    Some unnecessary files removed

--- dropdown5.js
+++
@@ -1,7 +0,0 @@
-function showSelectedOption() {
-    var selectMenu = document.getElementById("select-menu");
-    var selectedOption = selectMenu.options[selectMenu.selectedIndex].text;
-    var dialog = document.getElementById("dialog")
-    dialog.innerHTML = "Option " + selectedOption + " selected";
-    dialog.style.display = "block";
-}
